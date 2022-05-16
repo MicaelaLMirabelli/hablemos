99377fe3f2a0665834eda937772f267417782f74
Micaela Mirabelli <mirabellimicaela@gmail.com>
2022-05-16 20:00:19 -0300

    improve javascript

--- public/script.js
+++ public/script.js
@@ -1,29 +1,18 @@
 const fontSizeLargerButton = document.getElementById("font-size-larger");
 const fontSizeSmallerButton = document.getElementById("font-size-smaller");
 
-fontSizeLargerButton.addEventListener("click", incrementFontSize);
-fontSizeSmallerButton.addEventListener("click", dicreaseFontSize);
+fontSizeLargerButton.addEventListener("click", () => { changeTextFontSize(2) });
+fontSizeSmallerButton.addEventListener("click", () => { changeTextFontSize(-2) });
 
-function incrementFontSize() {
+function changeTextFontSize(amount) {
     const titleNodes = [...document.getElementsByClassName("title")];
     const optionsNodes = [...document.querySelectorAll("section li")];
-    const titleAndOptionNodes = titleNodes.concat(optionsNodes);
+    const buttonsNodes = [...document.getElementsByClassName("asidebutton")];
+    const textNodes = titleNodes.concat(optionsNodes, buttonsNodes);
 
-    titleAndOptionNodes.forEach(textNode => {
+    textNodes.forEach(textNode => {
         var style = window.getComputedStyle(textNode, null).getPropertyValue('font-size');
         var fontSize = parseFloat(style);
-        textNode.style.fontSize = fontSize + 2 + "px";
-    });
-}
-
-function dicreaseFontSize() {
-    const titleNodes = [...document.getElementsByClassName("title")];
-    const optionsNodes = [...document.querySelectorAll("section li")];
-    const titleAndOptionNodes = titleNodes.concat(optionsNodes);
-
-    titleAndOptionNodes.forEach(textNode => {
-        var style = window.getComputedStyle(textNode, null).getPropertyValue('font-size');
-        var fontSize = parseFloat(style);
-        textNode.style.fontSize = fontSize - 2 + "px";
+        textNode.style.fontSize = fontSize + amount + "px";
     });
 }
