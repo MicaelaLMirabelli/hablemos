3d4cb7bc016bae29a01098dca4b68c45ac2069e1
Micaela Mirabelli <mirabellimicaela@gmail.com>
2022-06-05 18:19:18 -0300

    resproduce sounds when click on button

--- public/script.js
+++ public/script.js
@@ -10,9 +10,27 @@ function changeTextFontSize(amount) {
     const buttonsNodes = [...document.getElementsByClassName("asidebutton")];
     const textNodes = titleNodes.concat(optionsNodes, buttonsNodes);
 
-    textNodes.forEach(textNode => {
+    textNodes.forEach((textNode) => {
         var style = window.getComputedStyle(textNode, null).getPropertyValue('font-size');
         var fontSize = parseFloat(style);
-        textNode.style.fontSize = fontSize + amount + "px";
+        textNode["style"]["fontSize"] = fontSize + amount + "px";
     });
 }
+
+const optionsButtons = [...document.querySelectorAll("section li")];
+const yesNoButtons = [...document.getElementsByClassName("asidebutton")];
+const phraseButtons = optionsButtons.concat(yesNoButtons);
+var synth = window.speechSynthesis;
+var voices = [];
+
+phraseButtons.forEach((phraseButton) => {
+    phraseButton.addEventListener("click", () => {
+        const speech = phraseButton.parentElement.dataset.phraseBeginning + " " + phraseButton.textContent.trim().toLowerCase();
+        var toSpeak = new SpeechSynthesisUtterance(speech);
+        toSpeak.voice = voices[7]; // Spanish
+        console.log(speech);
+        synth.cancel(toSpeak);
+        synth.speak(toSpeak);
+    });
+});
+
